b1be7a546a63a14c838d6ba626518b12a8aec880
@gosek <44835811+michalgosek@users.noreply.github.com>
2021-06-10 18:27:14 +0200

    Snake gameplay controller (#4)
    * moving up,down snake

--- Snake3D/src/game/SnakeBody.h
+++ Snake3D/src/game/SnakeBody.h
@@ -5,12 +5,15 @@ private:
 	float xPos;
 	float yPos;
 	float zPos;
+	char direction;
 
 public:
 	float GetXPos();
 	float GetYPos();
 	float GetZPos();
+	char GetDirection();
 	void SetXPos(float x);
 	void SetYPos(float y);
 	void SetZPos(float z);
+	void SetDirection(char direct);
 };
