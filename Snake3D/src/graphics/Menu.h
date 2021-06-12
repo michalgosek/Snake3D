e5611e7d9ab263a2ebd4cebb49a6f6b8ff3c01d0
mati0598 <mati0598@tlen.pl>
2021-06-12 09:15:09 +0200

    Add Auit Menu

--- Snake3D/src/graphics/Menu.h
+++ Snake3D/src/graphics/Menu.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "freeglut.h"
+#include "../game/Snake.h"
 
 class Menu
 {
@@ -8,8 +9,11 @@ class Menu
 private:
 	//static bool gameStart; 
 	static void drawMenu();
+	static void drawQuitMenu();
+	
 public:
 	static bool gameStart;
+	static bool gameBreak;
 	static void DisplayFunc();
 	static void Keyboard(unsigned char key, int x, int y);
 
